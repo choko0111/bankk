c676cf05403766e07bfac25ed15ccea1da1b7c7d
choko0111 <yofu1999@mail.ru>
2026-01-02 18:16:03 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-IMAGE_NAME=bank-tests
-
-echo ">>> Сборка Docker-образа: $IMAGE_NAME"
-docker build -t $IMAGE_NAME -f ./Dockerfile .
-
-# new
 echo ">>> Запуск тестов"
-
-docker network create bank-network
-docker run --rm --network bank-network \
-  -e BACKEND_URL=http://backend:4111/api \
-  bank-tests:latest
+docker-compose up --build --abort-on-container-exit
--- src/main/api/requests/create_user_requester.py
+++ src/main/api/requests/create_user_requester.py
@@ -5,7 +5,7 @@ from src.main.api.models.create_user_response import CreateUserResponse
 from src.main.api.requests.requester import Requester
 import requests
 
-# 6961
+# 69613
 class CreateUserRequester(Requester):
     def post(self, create_user_request: CreateUserRequest) -> CreateUserResponse | Response:
         url=f"{self.base_url}/admin/create"
