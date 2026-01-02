9faaed80660acfbe4acfcaa2603c7e4bf7d004cd
choko0111 <yofu1999@mail.ru>
2026-01-02 18:12:40 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,8 @@ docker build -t $IMAGE_NAME -f ./Dockerfile .
 
 # new
 echo ">>> Запуск тестов"
+
+docker network create bank-network
 docker run --rm --network bank-network \
   -e BACKEND_URL=http://backend:4111/api \
-  $IMAGE_NAME
+  bank-tests:latest
