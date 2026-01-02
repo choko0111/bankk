cc7c88bf481efa1422cb36bc1fbcc6787a7a3f65
choko0111 <yofu1999@mail.ru>
2026-01-02 17:50:06 +0300

    Update infra + check

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME=bank-tests
 
 echo ">>> Сборка Docker-образа: $IMAGE_NAME"
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f ./Dockerfile .
 
 echo ">>> Запуск тестов"
 docker run --rm $IMAGE_NAME
