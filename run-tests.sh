9fa1b57766004659eda496f36e40ac96cdd1f416
choko0111 <yofu1999@mail.ru>
2026-01-04 00:43:46 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -8,9 +8,4 @@ docker build -t $IMAGE_NAME .
 COMPOSE_NETWORK=test-bank_bank-network
 
 echo ">>> Запуск тестов"
-docker run --rm \
-  --network bank-network \
-  $IMAGE_NAME
-  bash -c "\
-    pytest --junitxml=/app/reports/raw/results.xml --tb=short \
-  "
+docker run --rm --network bank-network $IMAGE_NAME bash -c "pytest --junitxml=/app/reports/raw/results.xml --tb=short"
