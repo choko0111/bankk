bdb06cc2ab10c986bb6d4d76e67bc24fbea144e1
choko0111 <yofu1999@mail.ru>
2026-01-04 00:21:34 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -5,10 +5,10 @@ IMAGE_NAME=bank-tests
 echo ">>> Сборка Docker-образа: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
-COMPOSE_NETWORK=bank_net
+COMPOSE_NETWORK=test-bank_bank-network
 
 echo ">>> Запуск тестов в сети: $COMPOSE_NETWORK"
-
-docker run --rm --network test-net \
-  -e BACKEND_URL=http://bank_api:4111 \
+docker run --rm \
+  --network $COMPOSE_NETWORK \
+  -e BACKEND_URL=http://backend:4111/api \
   $IMAGE_NAME
