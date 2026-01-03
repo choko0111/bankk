cfe09859f805cb7e9cbb536cec514b73b8e6171f
choko0111 <yofu1999@mail.ru>
2026-01-04 00:49:22 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,14 @@ IMAGE_NAME=bank-tests
 echo ">>> Сборка Docker-образа: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
-COMPOSE_NETWORK=test-bank_bank-network
+# Получаем имя текущей папки (проекта)
+PROJECT_NAME=$(basename "$(pwd)")
 
-echo ">>> Запуск тестов"
-docker run --rm --network bank-network $IMAGE_NAME bash -c "pytest --junitxml=/app/reports/raw/results.xml --tb=short"
+# Формируем правильное имя сети
+COMPOSE_NETWORK="${PROJECT_NAME}_bank-network"
+
+echo ">>> Проверяем существующие сети"
+docker network ls | grep bank
+
+echo ">>> Запуск тестов в сети: $COMPOSE_NETWORK"
+docker run --rm --network "$COMPOSE_NETWORK" $IMAGE_NAME bash -c "pytest --junitxml=/app/reports/raw/results.xml --tb=short"
