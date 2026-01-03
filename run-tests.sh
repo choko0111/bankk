335960d3dd22f8e4955901982c2fd611495fe897
choko0111 <yofu1999@mail.ru>
2026-01-04 00:57:18 +0300

    Update infra + check 1

--- run-tests.sh
+++ run-tests.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+set -e
 
 IMAGE_NAME=bank-tests
 
 echo ">>> Сборка Docker-образа: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
-# Получаем имя текущей папки (проекта)
-PROJECT_NAME=$(basename "$(pwd)")
-
-# Формируем правильное имя сети
-COMPOSE_NETWORK="${PROJECT_NAME}_bank-network"
-
 echo ">>> Проверяем существующие сети"
-docker network ls | grep bank
+docker network ls
 
-echo ">>> Запуск тестов в сети: $COMPOSE_NETWORK"
-docker run --rm --network "$COMPOSE_NETWORK" $IMAGE_NAME bash -c "pytest --junitxml=/app/reports/raw/results.xml --tb=short"
+echo ">>> Используем сеть 'bank-network' (где реально находится backend)"
+docker run --rm \
+  --network bank-network \  # ← меняем на bank-network!
+  -e BACKEND_URL="http://backend:4111/api" \
+  $IMAGE_NAME \
+  bash -c "pytest --junitxml=/app/reports/raw/results.xml --tb=short"
